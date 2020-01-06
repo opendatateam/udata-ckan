d2564717cfaab623ac863ff061e7977677e23115
Alexandre Bulté <alexandre@bulte.net>
2020-01-06 14:53:12 +0100

    Bump version 1.3.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,6 @@
 # Changelog
 
-## Current (in progress)
+## 1.3.0 (2020-01-06)
 
 - DKAN support [#129](https://github.com/opendatateam/udata-ckan/pull/129)
 
--- udata_ckan/__init__.py
+++ udata_ckan/__init__.py
@@ -4,5 +4,5 @@ CKAN integration for udata
 '''
 from __future__ import unicode_literals
 
-__version__ = '1.2.4.dev'
+__version__ = '1.3.0'
 __description__ = 'CKAN integration for udata'
