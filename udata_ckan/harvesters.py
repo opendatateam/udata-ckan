1b62e4b4a4a22fd72368f1a52034ebb6ebe46047
quaxsze <service@plum.eu.com>
2020-01-07 16:51:37 +0100

    fix merge

--- udata_ckan/harvesters.py
+++ udata_ckan/harvesters.py
@@ -23,79 +23,8 @@ from .schemas.dkan import schema as dkan_schema
 
 log = logging.getLogger(__name__)
 
-RESOURCE_TYPES = ('file', 'file.upload', 'api', 'documentation',
-                  'image', 'visualization')
-
-ALLOWED_RESOURCE_TYPES = ('file', 'file.upload', 'api', 'metadata')
-
-resource = {
-    'id': str,
-    'position': int,
-    'name': All(DefaultTo(''), str),
-    'description': All(str, normalize_string),
-    'format': All(str, Lower),
-    'mimetype': Any(All(str, Lower), None),
-    'size': Any(Coerce(int), None),
-    'hash': Any(All(str, hash), None),
-    'created': All(str, to_date),
-    'last_modified': Any(All(str, to_date), None),
-    'url': All(str, is_url()),
-    'resource_type': All(empty_none,
-                         DefaultTo('file'),
-                         str,
-                         Any(*RESOURCE_TYPES)
-                         ),
-}
-
-tag = {
-    'id': str,
-    'vocabulary_id': Any(str, None),
-    'display_name': str,
-    'name': All(str, normalize_tag),
-    'state': str,
-}
-
-organization = {
-    'id': str,
-    'description': str,
-    'created': All(str, to_date),
-    'title': str,
-    'name': All(str, slug),
-    'revision_timestamp': All(str, to_date),
-    'is_organization': boolean,
-    'state': str,
-    'image_url': str,
-    'revision_id': str,
-    'type': 'organization',
-    'approval_status': 'approved'
-}
-
-schema = Schema({
-    'id': str,
-    'name': str,
-    'title': str,
-    'notes': Any(All(str, normalize_string), None),
-    'license_id': All(DefaultTo('not-specified'), str),
-    'license_title': Any(str, None),
-    'tags': [tag],
-
-    'metadata_created': All(str, to_date),
-    'metadata_modified': All(str, to_date),
-    'organization': Any(organization, None),
-    'resources': [resource],
-    'revision_id': str,
-    Optional('extras', default=list): [{
-        'key': str,
-        'value': Any(str, int, float, boolean, dict, list),
-    }],
-    'private': boolean,
-    'type': 'dataset',
-    'author': Any(str, None),
-    'author_email': All(empty_none, Any(All(str, email), None)),
-    'maintainer': Any(str, None),
-    'maintainer_email': All(empty_none, Any(All(str, email), None)),
-    'state': Any(str, None),
-}, required=True, extra=True)
+# dkan is a dummy value for dkan that does not provide resource_type
+ALLOWED_RESOURCE_TYPES = ('dkan', 'file', 'file.upload', 'api', 'metadata')
 
 
 class CkanBackend(BaseBackend):
