30baf993d6c539971bb1c31f195227161d2c71a6
maudetes <maudet.estelle@gmail.com>
2022-07-20 12:14:44 +0200

    Replace mongo legacy image in CI (#219)

--- tests/test_dkan_backend.py
+++ tests/test_dkan_backend.py
@@ -16,9 +16,6 @@ from udata.settings import Defaults, Testing
 from udata.tests.plugin import drop_db
 
 
-DKAN_TEST_INSTANCE = 'http://demo.getdkan.com'
-
-
 def data_path(filename):
     '''Get a test data path'''
     return os.path.join(os.path.dirname(__file__), 'data', filename)
@@ -37,38 +34,6 @@ def app(request):
     yield app
     with app.app_context():
         drop_db(app)
-
-
-@pytest.fixture(scope='module')
-def source(app):
-    '''
-    Create an harvest source for an organization.
-    The source is created once for the module.
-    '''
-    with app.app_context():
-        org = OrganizationFactory()
-        return HarvestSourceFactory(backend='dkan',
-                                    url=DKAN_TEST_INSTANCE,
-                                    organization=org)
-
-
-def test_dkan_demo_harvest(source, app):
-    '''
-    Harvest DKAN_TEST_INSTANCE and check some datasets are created
-    '''
-    with app.app_context():
-        actions.run(source.slug)
-        source.reload()
-        job = source.get_last_job()
-
-    assert len(job.items) > 0
-    datasets = Dataset.objects.filter(organization=source.organization)
-    assert len(job.items) == datasets.count()
-
-    for dataset in datasets:
-        assert len(dataset.resources) > 0
-
-    assert job.status == 'done'
 
 
 def test_dkan_french_w_license(app, rmock):
